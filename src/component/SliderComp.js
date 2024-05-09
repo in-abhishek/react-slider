6978c7386e4c59971267c82629a9ec7ba024fcbb
Abhishek-1607 <abhishek16singh07@gmail.com>
2024-05-09 17:21:08 +0530

    first commit

--- src/component/SliderComp.js
+++ src/component/SliderComp.js
@@ -50,8 +50,8 @@ const SliderComp = () => {
     return (
         <div className="parent">
             {Item === 0 ? <button type="button" className="showLess" onClick={handleLoadPrev} disabled>Previous</button> : <button type="button" className="showLess" onClick={handleLoadPrev}>Previous</button>}
-            {
-                listItems < data.length ?
+    
+    
                     <Slider {...settings} className={setSHowNext ? `slider-parent show` : `slider-parent`}>
                         {
                             data.slice(Item, listItems).map((singleData, key) => {
@@ -70,27 +70,7 @@ const SliderComp = () => {
                                 )
                             })
                         }
-                    </Slider> :  <Slider {...settings} className={setSHowNext ? `slider-parent show` : `slider-parent`}>
-                        {
-                            data.slice(Item, listItems).map((singleData, key) => {
-                                return (
-                                    <div className="singleContainer" key={key}>
-                                        <div className="card-container">
-                                            <div className="images">
-                                                <img src={singleData.image} alt="" className="image" />
-                                            </div>
-                                            <div className="content">
-                                                <h2>{singleData.title}</h2>
-                                                <p>{singleData.description}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
                     </Slider>
-            }
-
             {Item > data.length ? <button type="button" className="showMore" disabled onClick={handleLoadNext} >Next</button> : <button type="button" className="showMore" onClick={handleLoadNext} >Next</button>}
         </div>
     )
